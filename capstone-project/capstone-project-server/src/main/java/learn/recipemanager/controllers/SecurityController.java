d12f1a873d95cf777d43cb35768b8ce77509b3fe
kramiv <ikramarevsky@dev-10.com>
2022-10-10 10:57:23 -0500

    filled out AuthTokenFilter.java

--- capstone-project/capstone-project-server/src/main/java/learn/recipemanager/controllers/SecurityController.java
+++ capstone-project/capstone-project-server/src/main/java/learn/recipemanager/controllers/SecurityController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 
 
-//TODO do I need to specify origins?
+//TODO do I need to specify origins? Widget controller did, SolarPanel did not.
 @RestController
 @RequestMapping("api/security")
 @CrossOrigin( origins={"http://localhost:3000"} )
