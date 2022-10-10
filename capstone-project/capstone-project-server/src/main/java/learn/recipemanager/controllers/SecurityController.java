687f74c13193bc678636559cf17d89107ba70c87
kramiv <ikramarevsky@dev-10.com>
2022-10-10 17:47:35 -0500

    made code look prettier, addressed todo list. Will now implement global exception handler

--- capstone-project/capstone-project-server/src/main/java/learn/recipemanager/controllers/SecurityController.java
+++ capstone-project/capstone-project-server/src/main/java/learn/recipemanager/controllers/SecurityController.java
@@ -22,13 +22,9 @@ import java.util.List;
 @RestController
 @RequestMapping("api/security")
 public class SecurityController {
-
     private final AuthenticationManager authManager;
-
     private final AppUserService appUserService;
-
     private final JwtConverter converter;
-
     public SecurityController(AuthenticationManager authManager, AppUserService appUserService, JwtConverter converter) {
         this.authManager = authManager;
         this.appUserService = appUserService;
@@ -40,24 +36,17 @@ public class SecurityController {
         UsernamePasswordAuthenticationToken rawToken
                 = new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword());
         Authentication authentication = authManager.authenticate(rawToken);
-
         if (authentication.isAuthenticated()) {
             String jwtToken = converter.buildJwt((AppUser) authentication.getPrincipal());
-
             HashMap<String, String> tokenHolder = new HashMap<>();
             tokenHolder.put("jwt_token", jwtToken);
-
             return ResponseEntity.ok( tokenHolder );
         }
-
         return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-
     }
 
-    // 10/10/2022: sign-up functionality
     @PostMapping("/create_account")
     public ResponseEntity<?> createAccount(@RequestBody LoginRequest request) {//Map<String, String> credentials
-        //todo is it okay to use a map for credentials for create account?
         Result<AppUser> appUser = null;
 //removed try-catch: validationexception, duplicatekeyexception
         String username = request.getUsername();//this is email
@@ -66,18 +55,11 @@ public class SecurityController {
         if (!appUser.isSuccess()) {
             return new ResponseEntity<>(List.of(appUser.getMessages()), HttpStatus.BAD_REQUEST);
         }
-        // todo add duplicate error to validation
             // TODO: need to set up response entity and global exception handler
-//            return new ResponseEntity<>(List.of(ex.getMessage()), HttpStatus.BAD_REQUEST);
-//            return new ResponseEntity<>(List.of("The provided username already exists"), HttpStatus.BAD_REQUEST);
-
         //"happy path":
-        //todo make sure this part is set up correctly.
         HashMap<String, String> map = new HashMap<>();
         map.put("appUserId", appUser.getPayload().getUserId());
-
         return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
-
-
+    
 }
