dcf3768acafb28bc715623bbce4dc3cef8064e08
kramiv <ikramarevsky@dev-10.com>
2022-10-10 11:08:02 -0500

    filled out UserDetailsServiceImpl.java

--- capstone-project/capstone-project-server/src/main/java/learn/recipemanager/security/UserDetailsServiceImpl.java
+++ capstone-project/capstone-project-server/src/main/java/learn/recipemanager/security/UserDetailsServiceImpl.java
@@ -1,4 +1,23 @@
 package learn.recipemanager.security;
 
-public class UserDetailsServiceImpl {
+import learn.recipemanager.models.AppUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepo repo;
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        AppUser user = repo.getUserByUsername( username );
+        if( user == null ){
+            throw new UsernameNotFoundException("Could not find user " + username);
+        }
+        return user;
+    }
 }
