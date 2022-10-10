c88067f020414a9d94a1d89f3a67f58e3a1204ef
garretgvc <christiansongg@gmail.com>
2022-10-10 14:16:20 -0400

    update to loginhandler

--- capstone-project/capstone-project-client/src/App.js
+++ capstone-project/capstone-project-client/src/App.js
@@ -1,29 +1,33 @@
+import { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './HomePage/HomePage';
 import Login from './Login/Login';
 import NavBar from './NavBar/NavBar';
+import AuthContext from './AuthContext';
 
 function App() {
 
+  const [loginInfo, setLoginInfo] = useState(null);
+
   return (
+
     <div className="App">
-
-      <BrowserRouter>
-
-        <NavBar />
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="/login">
-            <Login/>
-          </Route>
-        </Switch>
-
-      </BrowserRouter>
-
+      <AuthContext.Provider value={loginInfo}>
+        <BrowserRouter>
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/login">
+              <Login setLoginInfo={setLoginInfo} />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </AuthContext.Provider>
     </div>
+
   );
 }
 
