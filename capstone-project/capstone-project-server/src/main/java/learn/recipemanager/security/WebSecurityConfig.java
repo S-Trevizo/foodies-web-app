c554bceb8e3c61abba12ccc8618566fd15d14f55
S-Trevizo <strevizo@protonmail.com>
2022-10-10 14:26:54 -0500

    updated securityconfig

--- capstone-project/capstone-project-server/src/main/java/learn/recipemanager/security/WebSecurityConfig.java
+++ capstone-project/capstone-project-server/src/main/java/learn/recipemanager/security/WebSecurityConfig.java
@@ -20,28 +20,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     AuthTokenFilter filter;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();    //turn off CORS checks TODO isn't this only good for when there are no forms?
+        http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS );
-        //http.exceptionHandling().authenticationEntryPoint(entryPoint);
+        
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST,  "/api/security/authenticate").permitAll()//HttpMethod.POST,
                 .antMatchers(HttpMethod.POST, "/api/security/create_account").permitAll()
-//                .antMatchers( HttpMethod.GET, "/api/widget/public" ).permitAll()
-//                .antMatchers( HttpMethod.GET, "/api/widget/personal/*").authenticated()
                 .anyRequest().denyAll();
         http.addFilterBefore( filter, UsernamePasswordAuthenticationFilter.class );
     }
-    //todo I think this throw is not related to mongo db throw. so
-    // We still need a global exception handler maybe.
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception{
         return super.authenticationManagerBean();
     }
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
+
 
 }
