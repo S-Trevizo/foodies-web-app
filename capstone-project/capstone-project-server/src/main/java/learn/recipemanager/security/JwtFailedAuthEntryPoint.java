cb0f72e9ffc0b192e36b24d53d6fea1d40100181
kramiv <ikramarevsky@dev-10.com>
2022-10-10 11:03:28 -0500

    filled out JwtFailedAuthEntryPoint.java

--- capstone-project/capstone-project-server/src/main/java/learn/recipemanager/security/JwtFailedAuthEntryPoint.java
+++ capstone-project/capstone-project-server/src/main/java/learn/recipemanager/security/JwtFailedAuthEntryPoint.java
@@ -1,4 +1,21 @@
 package learn.recipemanager.security;
 
-public class JwtFailedAuthEntryPoint {
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtFailedAuthEntryPoint implements AuthenticationEntryPoint {//add response to indicate failed login
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        response.sendError( HttpServletResponse.SC_UNAUTHORIZED, "Error: unauthorized user.");
+    }
 }
