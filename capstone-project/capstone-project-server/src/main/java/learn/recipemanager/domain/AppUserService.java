cc35dd9fe62b43a40727f79034d6c5cb70a4585d
S-Trevizo <strevizo@protonmail.com>
2022-10-10 11:32:06 -0500

    Added AppUser validation

--- capstone-project/capstone-project-server/src/main/java/learn/recipemanager/domain/AppUserService.java
+++ capstone-project/capstone-project-server/src/main/java/learn/recipemanager/domain/AppUserService.java
@@ -33,28 +33,71 @@ public class AppUserService implements UserDetailsService {
         return appUser;
     }
 
-    public AppUser create(String email, String password) {
-        validate(email);
-        validatePass(password);
+    public Result<AppUser> create(String email, String password) {
+
+        Result<AppUser> result = validate(email);
+
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        validatePass(password, result);
         AppRole appRole = new AppRole();
         appRole.setRoleName("User");
         password = encoder.encode(password);
 
         AppUser appUser = new AppUser( email, password, false, List.of(appRole));
+
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        result.setPayload(repo.save(appUser));
         
-        return repo.save(appUser);
+        return result;
     }
 
-    private void validatePass(String password) {
+    private Result<AppUser> validatePass(String password, Result<AppUser> result) {
 
+        if (password == null || password.length() < 8) {
+            result.addMessage("Password is required and should be more than 8 characters", ResultType.INVALID);
+            return result;
+        }
 
+        int digits = 0;
+        int letters = 0;
+        int others = 0;
+        for (char c : password.toCharArray()) {
+            if (Character.isDigit(c)) {
+                digits++;
+            } else if (Character.isLetter(c)) {
+                letters++;
+            } else {
+                others++;
+            }
+        }
+
+        if (digits == 0 || letters == 0 || others == 0) {
+            result.addMessage("password must contain a digit, a letter, and a non-digit/non-letter", ResultType.INVALID);
+        }
+
+        return result;
     }
 
-    private void validate(String email) {
+    private Result<AppUser> validate(String email) {
+
+        Result<AppUser> result = new Result<>();
 
         if (email == null || email.isBlank()) {
-
+            result.addMessage("Email is required", ResultType.INVALID);
+            return result;
         }
-        
+
+        String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+
+        if (email.matches(regex)) {
+            result.addMessage("Entry should be a proper email", ResultType.INVALID);
+        }
+        return result;
     }
 }
