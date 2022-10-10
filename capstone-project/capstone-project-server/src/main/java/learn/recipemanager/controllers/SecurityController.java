f32e948f1ef9d52d8ecd2cc7e40b8106b8cd4a99
kramiv <ikramarevsky@dev-10.com>
2022-10-10 13:31:19 -0500

    working on happy path case for creating an account

--- capstone-project/capstone-project-server/src/main/java/learn/recipemanager/controllers/SecurityController.java
+++ capstone-project/capstone-project-server/src/main/java/learn/recipemanager/controllers/SecurityController.java
@@ -2,10 +2,12 @@ package learn.recipemanager.controllers;
 
 
 import learn.recipemanager.domain.AppUserService;
+import learn.recipemanager.domain.Result;
 import learn.recipemanager.models.AppUser;
 import learn.recipemanager.models.viewmodels.LoginRequest;
 import learn.recipemanager.security.JwtConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,17 +16,18 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
+
 //crossorigins is set up in appconfig file
 @RestController
 @RequestMapping("api/security")
 public class SecurityController {
     @Autowired
-    AuthenticationManager authManager;
+    AuthenticationManager authManager;// todo currently, I think this is hard-coded. replace with class constructor later?
     @Autowired
     AppUserService appUserService;
     @Autowired
     JwtConverter converter;
-
     @PostMapping("/authenticate")
     ResponseEntity login(@RequestBody LoginRequest request){
         UsernamePasswordAuthenticationToken rawToken
@@ -37,4 +40,28 @@ public class SecurityController {
         tokenHolder.put("jwt_token", token);
         return ResponseEntity.ok( tokenHolder );
     }
+
+    // 10/10/2022: sign-up functionality
+    @PostMapping("/create_account")
+    public ResponseEntity<?> createAccount(@RequestBody LoginRequest request) {//Map<String, String> credentials
+        //todo is it okay to use a map for credentials for create account?
+        Result<AppUser> appUser = null;
+//removed try-catch: validationexception, duplicatekeyexception
+            String username = request.getUsername();//this is email
+            String password = request.getPassword();
+            appUser = appUserService.create(username, password);
+
+            // TODO: need to set up response entity and global exception handler
+//            return new ResponseEntity<>(List.of(ex.getMessage()), HttpStatus.BAD_REQUEST);
+//            return new ResponseEntity<>(List.of("The provided username already exists"), HttpStatus.BAD_REQUEST);
+
+        //"happy path":
+        //todo make sure this part is set up correctly.
+        HashMap<String, String> map = new HashMap<>();
+        map.put("appUserId", appUser.getPayload().getUserId());
+
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    }
+
+
 }
