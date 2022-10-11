cd43a358b878d47b7c190c9b9d3e558e26aae1a2
S-Trevizo <strevizo@protonmail.com>
2022-10-10 23:47:32 -0500

    made the select box for health labels

--- capstone-project/capstone-project-client/src/Register/Register.js
+++ capstone-project/capstone-project-client/src/Register/Register.js
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom';
 
+
+
 function Register() {
 
+    
 
     return (
 
@@ -14,17 +17,24 @@ function Register() {
                     <input id="name" name="name" className="form-control" disabled/>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="password">email (username):</label>
-                    <input id="password" name="password" className="form-control"/>
+                    <label htmlFor="email">Email (username):</label>
+                    <input id="email" name="email" className="form-control"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password:</label>
                     <input id="password" name="password" type="password " className="form-control" />
                 </div>
-                <div className="form-group">
-                    <label htmlFor="password">preferences</label>
-                    <input id="password" name="password" className="form-control" disabled/>
+                <div className='form-group'>
+                    
+                    <label htmlFor="healthLabel">Health Labels:</label>
+                    <select title='Select your preferences' actionsbox="true" data-live-search="true" className='selectpicker col-12' id='healthLabel' multiple>
+                        <option>Dairy Free</option>
+                        <option>Vegan</option>
+                        <option>Vegetarian</option> 
+                        {/* Need more options but this should work */}
+                    </select>
                 </div>
+                
 
                 <div className="text-right">
                     <button className="btn btn-primary ">Submit</button>
--- capstone-project/capstone-project-server/src/main/java/learn/recipemanager/security/WebSecurityConfig.java
+++ capstone-project/capstone-project-server/src/main/java/learn/recipemanager/security/WebSecurityConfig.java
@@ -44,18 +44,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:3000")
-//                        .allowedMethods("*");
-//            }
-//        };
-//    }
 
 
 }
