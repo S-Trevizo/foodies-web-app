ad9bea3464b9909be27f97028eb04ff5b96eb492
kramiv <ikramarevsky@dev-10.com>
2022-10-11 14:33:58 -0500

    printing error messages from external api fetch

--- capstone-project/capstone-project-client/src/Recipe/Recipe.js
+++ capstone-project/capstone-project-client/src/Recipe/Recipe.js
@@ -1,4 +1,15 @@
 function Recipe({recipeData}) {
+    /*
+    what information do I want to print out? 
+    recipeId
+    List<Ingredient>
+    link to directions
+    string of recipe name
+    string of allergens/health labels
+
+    --> move into bootstrap carousel
+
+    */
     //print out: 
     return <p>{recipeData.calories}
     </p>
--- capstone-project/capstone-project-client/src/Recipes/TwentyRandomRecipes.js
+++ capstone-project/capstone-project-client/src/Recipes/TwentyRandomRecipes.js
@@ -12,20 +12,20 @@ function TwentyRandomRecipes() {
 
     function apiFetch(input) {
         //what if q is missing? no output. How should I display that error to user?
-        fetch("https://api.edamam.com/api/recipes/v2?type=public&q=" + input.searchCriteria + "&app_id=4357d5e9&app_key=84496af29c091bb734dab8904e3d9df5", {
-            method: "GET",
+        // fetch("https://api.edamam.com/api/recipes/v2?type=public&q=" + input.searchCriteria + "&app_id=4357d5e9&app_key=84496af29c091bb734dab8904e3d9df5", {
+            fetch("https://api.edamam.com/api/recipes/v2?type=public&q=searchCriteria&app_id=4357d5e9&app_key=84496af29c091bb734dab8904e3d9df5&health=alcohol-cocktail&health=alcohol-free&health=celery-free&health=crustacean-free&health=dairy-free&health=DASH&health=egg-free&health=fish-free&health=fodmap-free&health=gluten-free&health=immuno-supportive&health=keto-friendly&health=kidney-friendly&health=kosher&health=low-fat-abs&health=low-potassium&health=low-sugar&health=lupine-free&health=Mediterranean&health=mollusk-free&health=mustard-free&health=no-oil-added&health=paleo&health=peanut-free&health=pescatarian&health=pork-free&health=red-meat-free&health=sesame-free&health=shellfish-free&health=soy-free&health=sugar-conscious&health=sulfite-free&health=tree-nut-free&health=vegan&health=vegetarian&health=wheat-free", {
+        method: "GET",
             headers: {
                 "Content-Type": "application/json"
             }
         }).then(async response => {
             if (response.status === 200) {
                 const toReturn = response.json();
-                // console.log(toReturn);
                 return toReturn;
             } else if (response.status === 400) {
-                //append to errors array
+                return Promise.reject(await response.json());
             } else if (response.status === 403) {
-                //append to errors array
+                return Promise.reject(await response.json());
             } else {
                 return Promise.reject(await response.json());
             }
@@ -37,12 +37,10 @@ function TwentyRandomRecipes() {
                 const copyArray = [];
                 copyArray.push("Could not connect to api.");
                 setErrorsToAppend(copyArray);
-                console.log(errorsToAppend);
             } else {
                 const copyArray = [];
                 copyArray.push(...error);
                 setErrorsToAppend(copyArray);
-                console.log(errorsToAppend);
             }
         });
     }
@@ -87,7 +85,9 @@ function TwentyRandomRecipes() {
 
             <div>
 {/* map: current value, current index, entire list */}
-{/* need to give key to recipec omponents */}
+{/* need to give key to recipec omponents 
+I want to make the id/key the uri or whatever it was. string select.*/}
+                {console.log(errorsToAppend)}
                 {recipes.map((r, index) => <Recipe key={index} recipeData={r.recipe}/>)}
                 if the recipe array is not empty, then call recipe.js and print out the desired values. 
                 if it is empty, then call the errors to display component
