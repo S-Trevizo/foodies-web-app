977debaf0d12970dff8c726430497d2ca223ff5e
kramiv <ikramarevsky@dev-10.com>
2022-10-11 10:18:59 -0500

    finished getRecipe for 20 random recipes

---
+++ capstone-project/capstone-project-server/src/main/java/learn/recipemanager/models/viewmodels/SearchCriteria.java
@@ -0,0 +1,13 @@
+package learn.recipemanager.models.viewmodels;
+
+public class SearchCriteria {
+    String searchCriteria;
+
+    public String getSearchCriteria() {
+        return searchCriteria;
+    }
+
+    public void setSearchCriteria(String searchCriteria) {
+        this.searchCriteria = searchCriteria;
+    }
+}
