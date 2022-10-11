b91c0c5352344752b57def71ed0d6dda81770f0c
kramiv <ikramarevsky@dev-10.com>
2022-10-11 15:31:10 -0500

    working error message

--- capstone-project/capstone-project-client/src/ErrorMessages/ErrorMessages.js
+++ capstone-project/capstone-project-client/src/ErrorMessages/ErrorMessages.js
@@ -1,14 +1,13 @@
 
 function ErrorMessages(props) {
-    //    console.log(props.errorData); works for "could not connect to api"
-    console.log(props.errorData.message);
     return (
-{ 
-}
-
-
-    <p>{props}
-    </p>
+        <>
+            {props.errorData.message ?
+                <div>{props.errorData.message}</div>
+                :
+                <div>{props.errorData}</div>
+            }
+        </>
     );
 }
 
