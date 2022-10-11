ee412cdad6c728413ed4a6e4f20a7f158cb0ec23
kramiv <ikramarevsky@dev-10.com>
2022-10-11 10:20:10 -0500

    a little more

--- capstone-project/capstone-project-server/src/main/java/learn/recipemanager/controllers/RecipesController.java
+++ capstone-project/capstone-project-server/src/main/java/learn/recipemanager/controllers/RecipesController.java
@@ -15,10 +15,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("api/recipe")
 public class RecipesController {
-<<<<<<< HEAD
-=======
-
->>>>>>> 094e7655df8c3d7d28e2359cb26217ca32700a50
     @Autowired
     AppUserService service;
     // todo where is a better place to initialize these variables?
