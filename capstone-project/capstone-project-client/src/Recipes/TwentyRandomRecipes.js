af26abab9e860e5480d87642f1e11ba94d667640
kramiv <ikramarevsky@dev-10.com>
2022-10-11 10:55:42 -0500

    working on twenty recipes functionality

--- capstone-project/capstone-project-client/src/Recipes/TwentyRandomRecipes.js
+++ capstone-project/capstone-project-client/src/Recipes/TwentyRandomRecipes.js
@@ -3,13 +3,51 @@ import {useContext, useState, useEffect} from "react";
 import { useHistory } from "react-router-dom";
 import Recipe from "../Recipe/Recipe";
 
-function TwentyRecipeResults() {
+function TwentyRecipeResults(prop) {//prop is searchCriteria
     const [recipes, setRecipes] = useState([]);
+    const [errorsToAppend, setErrorsToAppend] = useState([]);
     // first, make an api request. ignore login status for now. 
     // const userData = useContext(AuthContext);
     const history = useHistory();
 
+    function makeRecipeQuery(prop) {
+        fetch( "http://localhost:8080/api/recipe/public", {
+            method: "POST",
+            body: JSON.stringify(prop),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(async response => {
+            if (response.status === 200) {
+                // history.push( "/agents" );
+
+                //make external api request here
+
+
+                return response.json();
+            } else {
+                return Promise.reject(await response.json());
+            }
+        }).catch(error => {
+            if (error instanceof TypeError) {
+                const copyArray = [];
+                copyArray.push("Could not connect to api.");
+                setErrorsToAppend(copyArray);
+            } else {
+                const copyArray = [];
+                copyArray.push(...error);
+                setErrorsToAppend(copyArray);
+            }
+        });
+    }
+
+
+
+
+
     //check for errors from api. If no errors, then setRecipes. 
+    //setErrors option too. I think api shows what those look like. 
+
 
     //figure out how to read/map them in Recipe.js.
     
