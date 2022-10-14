528bb1c0ac6a1ef5b78bc5e7aca940b33ff39218
kramiv <ikramarevsky@dev-10.com>
2022-10-13 19:05:28 -0500

    refactored the rest of carousel into its own component because it was bothering me

--- capstone-project/capstone-project-client/src/TwentyRandomRecipes/TwentyRandomRecipes.js
+++ capstone-project/capstone-project-client/src/TwentyRandomRecipes/TwentyRandomRecipes.js
@@ -4,6 +4,7 @@ import { useHistory } from "react-router-dom";
 import Recipe from "../Recipe/Recipe";
 import ErrorMessages from "../ErrorMessages/ErrorMessages";
 import RecipeCarouseItem from "../RecipeCarouseItem/RecipeCarouseItem";
+import CarouselBody from "../CarouselBody/CarouselBody";
 
 
 function TwentyRandomRecipes(props) {
@@ -100,31 +101,11 @@ function TwentyRandomRecipes(props) {
 
     return (
         <div className="container text-center">
+            
             {errorsToAppend.map((r, index) => <ErrorMessages key={index} errorData={r} />)}
 
             {(recipes.length > 0) ?
-                <div id="carouselExampleIndicators" className="carousel slide w-70 h-30" data-ride="carousel">
-                    <ol className="carousel-indicators">
-                        {recipes &&
-                            recipes.map((r, index) => <li data-target="#carouselExampleIndicators" key={index} data-slide-to={"" + index + ""} className={index === 0 ? "active" : ""} />)}
-
-                    </ol>
-                    <div className="carousel-inner">
-                        {recipes ?
-                            recipes.map((r, index) => <RecipeCarouseItem key={index} recipeData={r.recipe} isActive={index === 0} />) :
-                            <div>No recipes could be found.</div>
-                        }
-                    </div>
-                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Previous</span>
-                    </a>
-                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Next</span>
-                    </a>
-                </div>
-
+                <CarouselBody listOfObjects={recipes}/>
                 : <div>no recipes found</div>}
 
         </div>
