3bf8682cc04d970d9fff85a2fa573020e1e5e2e5
S-Trevizo <strevizo@protonmail.com>
2022-10-13 22:10:19 -0500

    small changes

--- capstone-project/capstone-project-server/src/main/java/learn/recipemanager/models/AppRole.java
+++ capstone-project/capstone-project-server/src/main/java/learn/recipemanager/models/AppRole.java
@@ -1,16 +1,5 @@
 package learn.recipemanager.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.List;
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class AppRole {
     private String roleName;
 
@@ -23,7 +12,4 @@ public class AppRole {
         this.roleName = roleName;
     }
 
-    public GrantedAuthority getAuthority() {
-        return new SimpleGrantedAuthority(roleName);
-    }
 }
